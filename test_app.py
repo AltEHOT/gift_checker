1c700251802fe46709f83696dfaf00d6d3f8962b
EHOT <64489479+AltEHOT@users.noreply.github.com>
2026-08-06 17:41:45 +0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,5 +1,8 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
+port = int(os.getenv("PORT", 8080))
 
 @app.route('/')
 def home():
@@ -8,3 +11,7 @@ def home():
 @app.route('/health')
 def health():
     return "OK", 200
+
+# ЭТО ОБЯЗАТЕЛЬНО для python test_app.py
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=port)
